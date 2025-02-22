43426f20a20870af894f9cf85233438286bf38b4
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-02-22 13:24:16 +0530

    Add TaskList component and integrate it into EmployeeDashboard; update task counts and labels in TaskListNumber

--- Employee-Management-System/src/components/Dashboard/EmployeeDashbord.jsx
+++ Employee-Management-System/src/components/Dashboard/EmployeeDashbord.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../other/header'
 import TaskListNumber from '../../other/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 
 const EmployeeDashbord = () => {
@@ -8,6 +9,7 @@ const EmployeeDashbord = () => {
     <div className='p-10 bg-[#1C1C1C] h-screen'>
       <Header />
       <TaskListNumber />
+      <TaskList />
     </div>
   )
 }
--- Employee-Management-System/src/other/TaskListNumber.jsx
+++ Employee-Management-System/src/other/TaskListNumber.jsx
@@ -4,20 +4,20 @@ const TaskListNumber = () => {
   return (
     <div className='flex mt-10 justify-between gap-5 screen'>
         <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
-            <h2 className='text-3xl  font-semibold'>0</h2>
+            <h2 className='text-3xl  font-semibold'>1</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
         <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
-            <h2 className='text-3xl  font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h2 className='text-3xl  font-semibold'>3</h2>
+            <h3 className='text-xl font-medium'>Complete Task</h3>
         </div>
         <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
             <h2 className='text-3xl  font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Accept Task</h3>
         </div>
         <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-400'>
             <h2 className='text-3xl  font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Failed Task</h3>
         </div>
     </div>
   )
