64ba38fdeaa4d1700fc79d6a401479816f12f156
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-03-02 12:07:36 +0530

    Refactor AllTask component to dynamically render employee tasks from context and improve UI layout

--- Employee-Management-System/src/other/AllTask.jsx
+++ Employee-Management-System/src/other/AllTask.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthProvider'
 
 const AllTask = () => {
+    
+    const authData = useContext(AuthContext)
+    console.log(authData)
+    console.log(authData.employees)
   return (
-    <div id='tasklist' className='bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto'>
+    <div id='tasklist' className='bg-[#1c1c1c] p-5 rounded mt-5 '>
         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between items-center rounded'>
-            <h2>Prince</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
+            <h2 className='text-lg font-medium w-1/5 '>Employee Name </h2>
+            <h3 className='text-lg font-medium w-1/5 '>New Task</h3>
+            <h5 className='text-lg font-medium w-1/5 '>Active Task</h5>
+            <h5 className='text-lg font-medium w-1/5 '>Completed</h5>
+            <h5 className='text-lg font-medium w-1/5 '>Failed</h5>
         </div>
-        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between items-center rounded'>
-            <h2>Prince</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
+        <div >
+            {authData.employees && authData.employees.map((elem, index) => {
+                return (
+                    <div key={index} className='bg-emerald-500 mb-2 py-2 px-4 flex justify-between items-center rounded'>
+                        <h2 className='text-lg font-medium w-1/5'>{elem.firstName}</h2>
+                        <h3 className='text-lg font-medium w-1/5 '>{elem.taskCount.newTask}</h3>
+                        <h5 className='text-lg font-medium w-1/5'>{elem.taskCount.active}</h5>
+                        <h5 className='text-lg font-medium w-1/5'>{elem.taskCount.completedTask}</h5>
+                        <h5 className='text-lg font-medium w-1/5'>{elem.taskCount.failed}</h5>
+                    </div>
+                )
+             })}
         </div>
-        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between items-center rounded'>
-            <h2>Prince</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between items-center rounded'>
-            <h2>Prince</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between items-center rounded'>
-            <h2>Prince</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
+        
     </div>
 
   )
